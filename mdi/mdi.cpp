f1c4b67f4637e44cb9d4e76d3c797eb1f20c0620
Mark <markpk@users.noreply.github.com>
2017-06-06 13:08:12 +0200

    More work on glTF loader

--- mdi/mdi.cpp
+++ mdi/mdi.cpp
@@ -39,7 +39,7 @@ unsigned int indices[] = {
     0, 1, 2, 0, 1, 2
 };
 
-typedef  struct {
+typedef struct {
     GLuint  count;
     GLuint  instanceCount;
     GLuint  firstIndex;
@@ -49,7 +49,7 @@ typedef  struct {
 
 DrawElementsIndirectCommand commands[2];
 
-typedef struct { GLuint vbo, ibo; } GLMDIBuffers;
+typedef struct { GLuint vbo, ibo, cbo; } GLMDIBuffers;
 
 // From https://blog.nobel-joergensen.com/2013/02/17/debugging-opengl-part-2-using-gldebugmessagecallback/
 void APIENTRY openglCallbackFunction(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
@@ -194,11 +194,25 @@ static void window_close_callback(GLFWwindow* window)
 static void loadMesh(tinygltf::Scene &scene, GLuint progId, GLMDIBuffers &mdiBuffers) {
     cout << "Loading mesh..." << endl;
 
+    vector<DrawElementsIndirectCommand> commands;
+    map<string, int> viewsCommands; // maps bufferViews to index in the commands vector
+    map<string, int> viewsIndices;
+
     map<string, tinygltf::BufferView>::const_iterator it(scene.bufferViews.begin());
     map<string, tinygltf::BufferView>::const_iterator itEnd(scene.bufferViews.end());
 
     int vboSize = 0;
     int iboSize = 0;
+
+    int vboCount = 0;
+    int iboCount = 0;
+
+    // vec3 + vec3 + vec2 = 32 bytes
+    const int totalAttributesByteSize = 4 * 3 + 4 * 3 + 4 * 2;
+    const int elementsPerVertex = 3 + 3 + 2;
+
+    // Assuming indices are always unsigned ints
+    const int bytesPerIndex = 4;
 
     for (; it != itEnd; it++) {
         const tinygltf::BufferView &bufferView = it->second;
@@ -210,23 +224,31 @@ static void loadMesh(tinygltf::Scene &scene, GLuint progId, GLMDIBuffers &mdiBuf
         const tinygltf::Buffer &buffer = scene.buffers[bufferView.buffer];
         if (bufferView.target == ELEMENT_ARRAY_BUFFER) {
             iboSize += bufferView.byteLength;
+            iboCount++;
         }
         else if (bufferView.target == ARRAY_BUFFER) {
             vboSize += bufferView.byteLength;
+            vboCount++;
         }
     }
 
     cout << "bytes vbo: " << vboSize << endl;
     cout << "bytes ibo: " << iboSize << endl;
 
+    if (vboCount != iboCount) {
+        cout << "Error: expected equal number of vertex and index bufferViews" << endl;
+    }
+
     glCreateBuffers(1, &mdiBuffers.vbo);
     glCreateBuffers(1, &mdiBuffers.ibo);
+    glCreateBuffers(1, &mdiBuffers.cbo);
 
     // Create 1 big buffer for all the vertex data (attributes like position, normals, UV)
-    // and 1 buffer for all the indices
+    // and 1 buffer for all the indices, and 1 buffer for the draw commands
     // Note: assuming here that vertex attributes are interleaved
     glNamedBufferStorage(mdiBuffers.vbo, vboSize, NULL, GL_DYNAMIC_STORAGE_BIT);
     glNamedBufferStorage(mdiBuffers.ibo, iboSize, NULL, GL_DYNAMIC_STORAGE_BIT);
+    glNamedBufferStorage(mdiBuffers.cbo, vboCount * sizeof(DrawElementsIndirectCommand), NULL, GL_DYNAMIC_STORAGE_BIT);
 
     map<string, tinygltf::BufferView>::const_iterator it2(scene.bufferViews.begin());
     map<string, tinygltf::BufferView>::const_iterator it2End(scene.bufferViews.end());
@@ -234,22 +256,50 @@ static void loadMesh(tinygltf::Scene &scene, GLuint progId, GLMDIBuffers &mdiBuf
     int vboOffset = 0;
     int iboOffset = 0;
 
+    vboCount = 0;
+    iboCount = 0;
+
     // Copy data from buffer views to the VBO or IBO
     // Note: assuming here that vertex attributes are interleaved
     // (which means that for each mesh part, all its attributes fit in a single bufferView)
     for (; it2 != it2End; it2++) {
+        const string &name = it2->first;
         const tinygltf::BufferView &bufferView = it2->second;
 
         const tinygltf::Buffer &buffer = scene.buffers[bufferView.buffer];
         if (bufferView.target == ELEMENT_ARRAY_BUFFER) {
-            glNamedBufferSubData (mdiBuffers.ibo, vboOffset, bufferView.byteLength,
+            // Copy data to the next free location in the IBO
+            glNamedBufferSubData (mdiBuffers.ibo, iboOffset, bufferView.byteLength,
                 &buffer.data.at(0) + bufferView.byteOffset);
-            vboOffset += bufferView.byteLength;
+
+            // We cannot assume bufferViews for vertex attributes and indices
+            // are adjacent (in practice they are). So lets just store
+            // the current iboOffset / bytesPerIndex and then look up the correct command later
+            viewsIndices.insert(pair<string,int>(name, iboOffset / bytesPerIndex));
+
+            iboOffset += bufferView.byteLength;
+            iboCount++;
         }
         else if (bufferView.target == ARRAY_BUFFER) {
-            glNamedBufferSubData (mdiBuffers.vbo, iboOffset, bufferView.byteLength,
+            // Copy data to the next free location in the VBO 
+            glNamedBufferSubData (mdiBuffers.vbo, vboOffset, bufferView.byteLength,
                 &buffer.data.at(0) + bufferView.byteOffset);
-            iboOffset += bufferView.byteLength;
+
+            DrawElementsIndirectCommand command;
+
+            // Fill in count and firstIndex later
+            command.count = 0;
+            command.instanceCount = 1;
+            command.firstIndex = 0;
+            command.baseVertex = vboOffset / totalAttributesByteSize * elementsPerVertex;
+            command.baseInstance = vboCount;
+
+            commands.push_back(command);
+            cout << "Insert command for " << name << " at " << commands.size() - 1 << endl;
+            viewsCommands.insert(pair<string,int>(name, commands.size() - 1));
+
+            vboOffset += bufferView.byteLength;
+            vboCount++;
         }
     }
 
@@ -259,7 +309,87 @@ static void loadMesh(tinygltf::Scene &scene, GLuint progId, GLMDIBuffers &mdiBuf
     for (; itMesh != itMeshEnd; itMesh++) {
         const tinygltf::Mesh &mesh = itMesh->second;
         cout << mesh.name << " has " << mesh.primitives.size() << " primitives" << endl;
+
+        if (mesh.primitives.size() != 1) {
+            // We expect 1 primitive for simplicity
+            // If a mesh has multiple primitives, then you need to disable
+            // the non-active UV's in Blender
+            cout << "Error: expected 1 primitive for mesh " << mesh.name << endl;
+        }
+
+        for (size_t primId = 0; primId < mesh.primitives.size(); primId++) {
+            const tinygltf::Primitive &primitive = mesh.primitives[primId];
+
+            // Verify primitive has indices
+            if (primitive.indices.empty()) {
+                cout << "Error: expected primitive to have indices for mesh " << mesh.name << endl;
+            }
+
+            // Verify mode is 4 (triangles)
+            if (primitive.mode != 4) {
+                cout << "Error: expected primitive with mode 4 for mesh " << mesh.name << endl;
+            }
+
+            // Verify attributes are interleaved by checking all accessors
+            // point to the same bufferView
+            const tinygltf::Accessor &posAccessor =
+                scene.accessors[primitive.attributes.find("POSITION")->second];
+            const tinygltf::Accessor &nomAccessor =
+                scene.accessors[primitive.attributes.find("NORMAL")->second];
+            const tinygltf::Accessor &texAccessor =
+                scene.accessors[primitive.attributes.find("TEXCOORD_0")->second];
+
+            if (nomAccessor.bufferView != posAccessor.bufferView
+                || texAccessor.bufferView != posAccessor.bufferView) {
+                cout << "Error: vertex attributes not interleaved for mesh " << mesh.name << endl;
+            }
+
+            // Verify that POSITION attribute is vec3
+            if (posAccessor.componentType != 5126 || posAccessor.type != TINYGLTF_TYPE_VEC3) {
+                cout << "Expected POSITION to be vec3" << mesh.name << endl;
+            }
+            // Verify that NORMAL attribute is vec3
+            if (nomAccessor.componentType != 5126 || nomAccessor.type != TINYGLTF_TYPE_VEC3) {
+                cout << "Expected NORMAL to be vec3" << mesh.name << endl;
+            }
+            // Verify that TEXCOORD_0 attribute is vec2
+            if (texAccessor.componentType != 5126 || texAccessor.type != TINYGLTF_TYPE_VEC2) {
+                cout << "Expected TEXCOORD_0 to be vec2" << mesh.name << endl;
+            }
+
+            // Build commands array here
+            // Now we need to look at the bufferView and figure out the baseVertex (vertex offset)
+            // Look at primitive.indices's bufferView to figure out count and firstIndex
+            const tinygltf::Accessor &indexAccessor =
+                scene.accessors[primitive.indices];
+
+            // Verify indices are unsigned int
+            if (indexAccessor.componentType != 5125 || indexAccessor.type != TINYGLTF_TYPE_SCALAR) {
+                cout << "Error: expected unsigned int indices for mesh " << mesh.name << endl;
+            }
+
+            cout << mesh.name << " is OK " << posAccessor.bufferView << " " << viewsCommands[posAccessor.bufferView] << endl;
+
+            DrawElementsIndirectCommand &command = commands.at(viewsCommands[posAccessor.bufferView]);
+            command.firstIndex = viewsIndices[indexAccessor.bufferView];
+            command.count = indexAccessor.count;
+        }
     }
+
+    // Debug: print commands
+
+    vector<DrawElementsIndirectCommand>::const_iterator itCommand(commands.begin());
+    vector<DrawElementsIndirectCommand>::const_iterator itCommandEnd(commands.end());
+
+    for (; itCommand != itCommandEnd; itCommand++) {
+        const DrawElementsIndirectCommand &command = *itCommand;
+
+        cout << "count: " << command.count << " instanceCount: " << command.instanceCount << " firstIndex: " << command.firstIndex << " baseVertex: " << command.baseVertex << " baseInstance: " << command.baseInstance << endl;
+    }
+
+    // Copy commands to CBO
+    glNamedBufferSubData(mdiBuffers.cbo, 0,
+        commands.size() * sizeof(DrawElementsIndirectCommand), &commands.at(0));
 }
 
 int main(int argc, char *argv[])
